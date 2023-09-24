9167f7e3d829c25fcd98ed7e8b71a73a7b6c3bfd
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-24 19:30:04 -0400

    Pushing to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ console.log('data saved to the database',result);
 export const getDb = async () =>{
   console.log('Get from the database');
   const contactDb = await openDB('jate',1);
-  const tx=contactDb.trasaction('jate','readonly');
+  const tx=contactDb.transaction('jate','readonly');
   const store=tx.objectStore('jate');
   const request =store.getAll();
   const result =await request;
